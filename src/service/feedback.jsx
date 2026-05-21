1c880d3701a221c28f6e3c559696dbbea89771ca
nashwa-27 <541251213@student.smktelkom-pwt.sch.id>
2026-05-21 19:23:51 +0700

    tambah feedback1

---
+++ src/service/feedback.jsx
@@ -0,0 +1,3 @@
+// Tambahkan baris ini SEMENTARA di paling bawah file untuk uji coba:
+insertUserFeedback("test@gmail.com", "Halo, ini pesan uji coba dari Backend!")
+  .then(res => console.log("Hasil Tes Backend:", res));
