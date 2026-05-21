3be000c0489e082f99ffd1da913afe10d3d45d26
nashwa-27 <541251213@student.smktelkom-pwt.sch.id>
2026-05-21 16:34:06 +0700

    update

--- src/service/auth.jsx
+++ src/service/auth.jsx
@@ -30,4 +30,27 @@ export const onSignUp = async (FirstName, LastName, UserEmail, Password, navigat
 
         navigate ("/");
 
+
 }
+//signup
+export const onSignIn = async (FirstName, LastName, UserEmail, Password) => {
+        const { data, error } = await supabase.auth.signUp({ 
+           
+        });
+
+        if (error) {
+            console.log("Error signing up: ", error.message);
+            return;
+        }
+}
+//login
+const { data, error } = await supabase.auth.signInWithPassword({
+  email,
+  password
+})
+//signout
+const { error: ErrorSignOut } = await supabase.auth.signOut()
+//chaptergame
+const { data, error } = await supabase
+  .from('characters')
+  .select()
