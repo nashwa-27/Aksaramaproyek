e347429e9db7682e19aab236d669e66f32d54a7f
nashwa-27 <541251213@student.smktelkom-pwt.sch.id>
2026-05-22 13:34:40 +0700

    perbaikan feedback

--- src/service/feedback.jsx
+++ src/service/feedback.jsx
@@ -1,3 +1,5 @@
+import { supabase } from "../lib/supabaseClient";
+
 // Tambahkan baris ini SEMENTARA di paling bawah file untuk uji coba:
 insertUserFeedback("test@gmail.com", "Halo, ini pesan uji coba dari Backend!")
   .then(res => console.log("Hasil Tes Backend:", res));
